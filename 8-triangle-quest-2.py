b1eae10fed908bffa22f07d23b166ec4d5995d0c
Daniel Fuentes Cea <dfuentesc@falabella.cl>
2019-11-23 01:43:58 -0300

    resolve triangle-quest-2 one line

--- 8-triangle-quest-2.py
+++ 8-triangle-quest-2.py
@@ -1,7 +1,3 @@
 if __name__ == '__main__':
-    n = int(input())
-
-    for i in range(1, n + 1):
+    for i in range(1,int(input())+1): #More than 2 lines will result in 0 score. Do not leave a blank line also
         print (10 ** i / 9) ** 2
-        
-
